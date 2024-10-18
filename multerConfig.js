c58f347780d2cb6c33612fe7bf0860d595eff87d
Joshua-UND <142265673+Joshua-UND@users.noreply.github.com>
2024-10-18 17:56:58 +0100

    Update multerConfig.js

--- multerConfig.js
+++ multerConfig.js
@@ -1,34 +1,20 @@
 const multer = require('multer');
-const { GridFsStorage } = require('multer-gridfs-storage');
-const mongoose = require('mongoose');
 const path = require('path');
 
-// MongoDB connection URI (use your own MongoDB URI)
-const mongoURI = MONGODB_URI;
-
-// Create storage engine
-const storage = new GridFsStorage({
-    url: mongoURI,
-    file: (req, file) => {
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
         const { matricNumber, level, semester } = req.body;
-        
-        // Set the filename format
+
         const fileExtension = path.extname(file.originalname);
         const fileName = `${matricNumber}-${level}-${semester}${fileExtension}`;
-
-        return {
-            filename: fileName,
-            metadata: {
-                matricNumber,
-                level,
-                semester
-            },
-            bucketName: 'uploads' // This is the collection name for the files
-        };
+        
+        cb(null, fileName);
     }
 });
 
-// Set up the multer upload configuration
 const upload = multer({ storage });
 
 module.exports = upload;
